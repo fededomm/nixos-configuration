7cc958687e4c18c7e553c67eb20765a814b0852e
fededomm <fededomm@outlook.it>
2024-01-13 18:08:41 +0100

    Generation label: add_vim_test

--- configuration.nix
+++ configuration.nix
@@ -118,6 +118,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    vim
     jetbrains.idea-ultimate
     jetbrains.goland
     jetbrains.datagrip
