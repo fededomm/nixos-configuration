0b00a099911f9c9210741ba43b4885023fc18770
fededomm <fededomm@outlook.it>
2024-01-15 17:11:18 +0100

    remove hardware config copy in copyback.sh

--- copyback.sh
+++ copyback.sh
@@ -5,5 +5,5 @@ user="fededomm"
 home="/home/$user"
 
 sudo cp $home/Desktop/nixos-configuration/configuration.nix /etc/nixos/
-sudo cp $home/Desktop/nixos-configuration/hardware-configuration.nix /etc/nixos/
+#sudo cp $home/Desktop/nixos-configuration/hardware-configuration.nix /etc/nixos/
 sudo cp -r $home/Desktop/nixos-configuration/sub-configuration /etc/nixos/
