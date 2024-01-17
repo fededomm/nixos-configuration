1d780110c6284b864a4d23943672a3b93e6b8ab9
fededomm <fededomm@outlook.it>
2024-01-17 16:29:42 +0100

    Generation label: Install_kde_desktop_environment-0.1.0

---
+++ sub-configuration/desktop-environments/kde.nix
@@ -0,0 +1,11 @@
+{config, pkgs, ...}:
+
+{
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    plasma-browser-integration
+    konsole
+    oxygen
+  ];
+}
