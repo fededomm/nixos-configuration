205ca1e22280905b82eab6ff3de7201cf22fb162
fededomm <fededomm@outlook.it>
2024-01-17 17:04:05 +0100

    Generation label: try_to_fix_kde-0.1.0

--- sub-configuration/desktop-environments/kde.nix
+++ sub-configuration/desktop-environments/kde.nix
@@ -3,9 +3,5 @@
 {
   services.xserver.desktopManager.plasma5.enable = true;
 
-  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
-    plasma-browser-integration
-    konsole
-    oxygen
-  ];
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [];
 }
